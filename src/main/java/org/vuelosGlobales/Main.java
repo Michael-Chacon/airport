0b9109d53001b44c5a895cf1576b3ae553b67657
Michael-Chacon <michaelchacon02@gmail.com>
2024-06-29 14:24:46 -0500

    Migracion modulo reservas

--- src/main/java/org/vuelosGlobales/Main.java
+++ src/main/java/org/vuelosGlobales/Main.java
@@ -1,33 +1,11 @@
 package org.vuelosGlobales;
 
-import org.vuelosGlobales.generals.connection.adapter.out.ConnectionMySQLRepository;
-import org.vuelosGlobales.generals.passenger.adapter.out.PassengerMySQLRepo;
-import org.vuelosGlobales.generals.trip.adapter.out.TripMySQLRepository;
-import org.vuelosGlobales.salesAgent.customer.adapter.out.CustomerMySQLRepository;
-import org.vuelosGlobales.salesAgent.flightRes.adapter.in.FlightResConsoleAdapter;
-import org.vuelosGlobales.salesAgent.flightRes.adapter.out.FlightResMySQLRepository;
-import org.vuelosGlobales.salesAgent.flightRes.application.FlightResService;
-import org.vuelosGlobales.shared.Constants;
-import org.vuelosGlobales.systemAdministrator.document.adapter.out.DocumentMySQLRepository;
-import org.vuelosGlobales.systemAdministrator.fare.adapter.out.FareMySQLRepository;
+
+import org.vuelosGlobales.shared.Menus;
 
 public class Main {
     public static void main(String[] args) {
-        CustomerMySQLRepository customerOut = new CustomerMySQLRepository(Constants.URL, Constants.USER, Constants.PASSWORD);
-        TripMySQLRepository tripOut = new TripMySQLRepository(Constants.URL, Constants.USER, Constants.PASSWORD);
-        ConnectionMySQLRepository connOut = new ConnectionMySQLRepository(Constants.URL, Constants.USER, Constants.PASSWORD);
-        PassengerMySQLRepo passengerMySQLRepo = new PassengerMySQLRepo(Constants.URL, Constants.USER, Constants.PASSWORD);
-        FareMySQLRepository fareOut = new FareMySQLRepository(Constants.URL, Constants.USER, Constants.PASSWORD);
-        DocumentMySQLRepository documentOut = new DocumentMySQLRepository(Constants.URL, Constants.USER, Constants.PASSWORD);
-
-        FlightResMySQLRepository flightResMySQLRepository = new FlightResMySQLRepository(Constants.URL, Constants.USER, Constants.PASSWORD);
-        FlightResService flightResService = new FlightResService(flightResMySQLRepository, customerOut, tripOut, connOut, fareOut, passengerMySQLRepo, documentOut);
-        FlightResConsoleAdapter fligresIn = new FlightResConsoleAdapter(flightResService);
-
-        fligresIn.crudFlightRes();
-    /*    Menus menus = new Menus();
+    Menus menus = new Menus();
         menus.MenuPrincipal();
-        *
-     */
     }
 }
